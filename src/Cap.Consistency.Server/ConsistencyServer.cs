d9a60d3400def4696069639a4900ebcd4654c9d0
yuleyule66 <yangxiaodong1214@126.com>
2016-12-16 20:27:11 +0800

    add server project

---
+++ src/Cap.Consistency.Server/ConsistencyServer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.Extensions.Logging;
+
+namespace Cap.Consistency.Server
+{
+    public class ConsistencyServer : IServer
+    {
+        public ConsistencyServer(IApplicationLifetime applicationLifetime, ILoggerFactory loggerFactory)
+        {
+            if (applicationLifetime == null)
+            {
+                throw new ArgumentNullException(nameof(applicationLifetime));
+            }
+
+            if (loggerFactory==null)
+            {
+                throw  new ArgumentNullException(nameof(loggerFactory));
+            }
+        }
+
+        public void Start<TContext>(IHttpApplication<TContext> application)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IFeatureCollection Features { get; }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Cap.Consistency.Server/Properties/AssemblyInfo.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Cap.Consistency.Server")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cap.Consistency.Server")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("55CF2C48-D390-40CF-8AD9-FA39F90E9217")]
