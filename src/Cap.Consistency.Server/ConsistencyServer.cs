612c63d1001525c0f1b8379be8a4ec730029a6ac
yangxiaodong <yangxiaodong1214@126.com>
2016-12-20 15:43:50 +0800

    addserver

--- src/Cap.Consistency.Server/ConsistencyServer.cs
+++ src/Cap.Consistency.Server/ConsistencyServer.cs
@@ -6,35 +6,76 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Hosting.Server;
 using Microsoft.AspNetCore.Http.Features;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using System.Reflection;
+using Cap.Consistency.Server.Internal.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Cap.Consistency.Server
 {
     public class ConsistencyServer : IServer
     {
-        public ConsistencyServer(IApplicationLifetime applicationLifetime, ILoggerFactory loggerFactory)
-        {
-            if (applicationLifetime == null)
-            {
+        private Stack<IDisposable> _disposables;
+        private readonly IApplicationLifetime _applicationLifetime;
+        private readonly ILogger _logger;
+        private readonly IServerAddressesFeature _serverAddresses;
+        private readonly IConsumer _consumer;
+
+        public ConsistencyServer(IOptions<ConsistencyServerOptions> options, IApplicationLifetime applicationLifetime, ILoggerFactory loggerFactory) {
+            if (options == null) {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            if (applicationLifetime == null) {
                 throw new ArgumentNullException(nameof(applicationLifetime));
             }
 
-            if (loggerFactory==null)
-            {
-                throw  new ArgumentNullException(nameof(loggerFactory));
+            if (loggerFactory == null) {
+                throw new ArgumentNullException(nameof(loggerFactory));
             }
-        }
 
-        public void Start<TContext>(IHttpApplication<TContext> application)
-        {
-            throw new NotImplementedException();
+            Options = options.Value ?? new ConsistencyServerOptions();
+            _applicationLifetime = applicationLifetime;
+            _logger = loggerFactory.CreateLogger(typeof(ConsistencyServer).GetTypeInfo().Namespace);
+            _consumer = Options.ApplicationServices.GetService<IConsumer>();
+            Features = new FeatureCollection();
+            _serverAddresses = new ServerAddressesFeature();
+            Features.Set(_serverAddresses);
         }
 
         public IFeatureCollection Features { get; }
 
-        public void Dispose()
-        {
-            
-            throw new NotImplementedException();
+        public ConsistencyServerOptions Options { get; }
+
+        public void Start<TContext>(IHttpApplication<TContext> application) {
+            if (_disposables != null) {
+                // The server has already started and/or has not been cleaned up yet
+                throw new InvalidOperationException("Server has already started.");
+            }
+            _disposables = new Stack<IDisposable>();
+            var trace = new ConsistencyTrace(_logger);
+
+            _disposables.Push(_consumer);
+
+            var threadCount = Options.ThreadCount;
+
+            if (threadCount <= 0) {
+                throw new ArgumentOutOfRangeException(nameof(threadCount),
+                    threadCount,
+                    "ThreadCount must be positive.");
+            }
+
+            _consumer.Start();
+        }
+
+        public void Dispose() {
+            if (_disposables != null) {
+                while (_disposables.Count > 0) {
+                    _disposables.Pop().Dispose();
+                }
+                _disposables = null;
+            }
         }
     }
 }
