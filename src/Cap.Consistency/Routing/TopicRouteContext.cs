87b26ae9be172a7db247ac70c196388480a63463
yangxiaodong <yangxiaodong1214@126.com>
2017-05-21 16:56:50 +0800

    refactor

--- src/Cap.Consistency/Routing/TopicRouteContext.cs
+++ src/Cap.Consistency/Routing/TopicRouteContext.cs
@@ -1,33 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using Cap.Consistency.Abstractions;
-using Cap.Consistency.Consumer;
-using Cap.Consistency.Infrastructure;
+using System.Collections.Generic; 
 
 namespace Cap.Consistency.Routing
 {
-    public delegate Task HandlerConsumer(ConsumerExecutorDescriptor context);
-
     public class TopicRouteContext
     {
-        public TopicRouteContext() {
-
-        }
-
-        public TopicRouteContext(DeliverMessage message) {
-            Message = message;
-        }
-
-        public DeliverMessage Message { get; }
-
-        // public event EventHandler<ConsumerExecutorDescriptor> OnMessage;
-
-        public HandlerConsumer Handler { get; set; }
-
-        public IList<IConsumerHandler> Consumers { get; set; }
-
         public IServiceProvider ServiceProvider { get; set; }
 
         public IList<ITopicRoute> Routes { get; set; }
