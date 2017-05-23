29d31f1b2ba697dcb5ca2b92487fceb23dba1eb8
yangxiaodong <yangxiaodong1214@126.com>
2017-05-23 16:58:45 +0800

    refactor

---
+++ samples/Sample.Kafka/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Cap.Consistency.Attributes;
+using Cap.Consistency.Consumer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Sample.Kafka.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller, IConsumerService
+    {
+
+        [KafkaTopic("zzwl.topic.finace.callBack", IsOneWay = true)]
+        public void KafkaTest() {
+            Console.WriteLine("kafka test invoked");
+        }
+    }
+}
