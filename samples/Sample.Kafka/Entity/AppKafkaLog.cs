9ef55b972ee1d8f17d67b7c5dcf0708ec51e21d6
yangxiaodong <yangxiaodong1214@126.com>
2017-05-23 16:58:50 +0800

    refactor

---
+++ samples/Sample.Kafka/Entity/AppKafkaLog.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample.Kafka.Entity
+{
+    public class AppKafkaLog
+    {
+    }
+}
