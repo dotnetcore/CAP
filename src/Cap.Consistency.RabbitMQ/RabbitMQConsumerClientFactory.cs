0476ed438f2152c97e320ec338374f1fe2891aff
yangxiaodong <yangxiaodong1214@126.com>
2017-05-26 18:33:18 +0800

    add rabbitmq proejct

---
+++ src/Cap.Consistency.RabbitMQ/RabbitMQConsumerClientFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cap.Consistency.Consumer;
+
+namespace Cap.Consistency.RabbitMQ
+{
+    public class RabbitMQConsumerClientFactory : IConsumerClientFactory
+    {
+        public IConsumerClient Create(string groupId, string clientHostAddress) {
+            return new RabbitMQConsumerClient(groupId, clientHostAddress);
+        }
+    }
+}
---
+++ src/Cap.Consistency.RabbitMQ/RabbitMQTopicAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cap.Consistency.Abstractions;
+
+namespace Cap.Consistency.RabbitMQ
+{
+    public class RabbitMQTopicAttribute : TopicAttribute
+    {
+
+        public RabbitMQTopicAttribute(string routingKey) : base(routingKey) {
+
+        }
+    }
+}
