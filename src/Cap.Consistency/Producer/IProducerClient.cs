d2c77c07f5bbe98326aea552ce09fa217318c911
yangxiaodong <yangxiaodong1214@126.com>
2017-05-29 18:45:19 +0800

    add producer.

--- src/Cap.Consistency/Producer/IProducerClient.cs
+++ src/Cap.Consistency/Producer/IProducerClient.cs
@@ -1,13 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Cap.Consistency.Producer
 {
     public interface IProducerClient
     {
-
-
-
+        Task SendAsync(string topic, string content);
     }
 }
