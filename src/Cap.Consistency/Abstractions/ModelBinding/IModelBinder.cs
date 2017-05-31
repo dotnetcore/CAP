209bdcff68156eabb7dcb308439c55e61e842d6e
yangxiaodong <yangxiaodong1214@126.com>
2017-05-31 18:47:00 +0800

    add modelbinder.

---
+++ src/Cap.Consistency/Abstractions/ModelBinding/IModelBinder.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap.Consistency.Abstractions.ModelBinding
+{
+    public interface IModelBinder
+    {
+        Task BindModelAsync(ModelBindingContext bindingContext);
+    }
+}
---
+++ src/Cap.Consistency/Abstractions/ModelBinding/ModelBindingContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Primitives;
+
+namespace Cap.Consistency.Abstractions.ModelBinding
+{
+    public class ModelBindingContext
+    {
+        public object Model { get; set; }
+
+        public string ModelName { get; set; }
+
+        public Type ModelType { get; set; }
+
+        public StringValues Values { get; set; }
+
+        public object Result { get; set; }
+
+        public static ModelBindingContext CreateBindingContext(string values, string modelName, Type modelType) {
+            return new ModelBindingContext() {
+                ModelName = modelName,
+                ModelType = modelType,
+                Values = values
+            };
+        }
+    }
+}
