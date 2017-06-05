eda2ef1305ad752cd9ed8769cbe010d912b3c148
yangxiaodong <yangxiaodong1214@126.com>
2017-06-05 11:36:57 +0800

    refactor

---
+++ src/Cap.Consistency/Microsoft.Extensions.DependencyInjection/ConsistencyBuilder.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Used to verify Consistency service was called on a ServiceCollection
+    /// </summary>
+    public class ConsistencyMarkerService { }
+
+    public class ConsistencyBuilder
+    {
+        public ConsistencyBuilder(IServiceCollection services) {
+            Services = services;
+        }
+
+        public IServiceCollection Services { get; private set; }
+    }
+}
---
+++ src/Cap.Consistency/Microsoft.Extensions.DependencyInjection/ServiceCollectionExtensions.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Cap.Consistency;
+using Cap.Consistency.Abstractions.ModelBinding;
+using Cap.Consistency.Consumer;
+using Cap.Consistency.Infrastructure;
+using Cap.Consistency.Internal;
+using Cap.Consistency.Store;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+// ReSharper disable once CheckNamespace
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// Contains extension methods to <see cref="IServiceCollection"/> for configuring consistence services.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds and configures the consistence services for the consitence.
+        /// </summary>
+        /// <param name="services">The services available in the application.</param>
+        /// <returns>An <see cref="ConsistencyBuilder"/> for application services.</returns>
+        public static ConsistencyBuilder AddConsistency(this IServiceCollection services) {
+            services.AddConsistency(x => new ConsistencyOptions());
+
+            return new ConsistencyBuilder(services);
+        }
+
+        /// <summary>
+        /// Adds and configures the consistence services for the consitence.
+        /// </summary>
+        /// <param name="services">The services available in the application.</param>
+        /// <param name="setupAction">An action to configure the <see cref="ConsistencyOptions"/>.</param>
+        /// <returns>An <see cref="ConsistencyBuilder"/> for application services.</returns>
+        public static ConsistencyBuilder AddConsistency(this IServiceCollection services, Action<ConsistencyOptions> setupAction) {
+            services.TryAddSingleton<ConsistencyMarkerService>();
+
+            services.TryAddSingleton<ConsistencyMessageManager>();
+
+            services.Configure(setupAction);
+
+
+            var IConsumerListenerServices = new Dictionary<Type, Type>();
+            foreach (var rejectedServices in services) {
+                if (rejectedServices.ImplementationType != null && typeof(IConsumerService).IsAssignableFrom(rejectedServices.ImplementationType))
+                    IConsumerListenerServices.Add(typeof(IConsumerService), rejectedServices.ImplementationType);
+            }
+
+            foreach (var service in IConsumerListenerServices) {
+                services.AddSingleton(service.Key, service.Value);
+            }
+
+            var types = Assembly.GetEntryAssembly().ExportedTypes;
+            foreach (var type in types) {
+                if (typeof(IConsumerService).IsAssignableFrom(type)) {
+                    services.AddSingleton(typeof(IConsumerService), type);
+                }
+            }
+
+            services.TryAddSingleton<IConsumerExcutorSelector, ConsumerExcutorSelector>();
+            services.TryAddSingleton<IModelBinder, DefaultModelBinder>();
+            services.TryAddSingleton<IConsumerInvokerFactory, ConsumerInvokerFactory>();
+            services.TryAddSingleton<MethodMatcherCache>();
+
+            services.TryAddSingleton(typeof(ITopicRouteHandler), typeof(ConsumerHandler));
+
+            return new ConsistencyBuilder(services);
+        }
+
+
+        public static ConsistencyBuilder AddMessageStore<T>(this ConsistencyBuilder build)
+            where T : class, IConsistencyMessageStore {
+            build.Services.AddScoped<IConsistencyMessageStore, T>();
+            return build;
+        }
+    }
+}
