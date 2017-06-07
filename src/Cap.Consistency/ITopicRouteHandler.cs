dccff2acbb6ccbc7171fa836a1744b38b9e56dc7
yangxiaodong <yangxiaodong1214@126.com>
2017-06-07 11:37:17 +0800

    refactor

---
+++ src/Cap.Consistency/ITopicRouteHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cap.Consistency
+{
+    public interface ITopicRouteHandler
+    {
+        Task RouteAsync(TopicRouteContext context);
+    }
+}
---
+++ src/Cap.Consistency/TopicRouteContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic; 
+
+namespace Cap.Consistency
+{
+    public class TopicRouteContext
+    {
+        public IServiceProvider ServiceProvider { get; set; }
+
+        public IList<ITopicRouteHandler> Routes { get; set; }
+    }
+}
