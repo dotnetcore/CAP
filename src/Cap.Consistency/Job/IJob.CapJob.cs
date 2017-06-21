d52ce28824e6f3570fcf3998c1d12737450a7443
yangxiaodong <yangxiaodong1214@126.com>
2017-06-21 16:07:16 +0800

    add Capjob.

--- src/Cap.Consistency/Job/IJob.CapJob.cs
+++ src/Cap.Consistency/Job/IJob.CapJob.cs
@@ -8,13 +8,11 @@ namespace Cap.Consistency.Job
     public class CapJob : IJob
     {
 
-
-
-
         public Task ExecuteAsync() {
 
-            throw new NotImplementedException();
+            Console.WriteLine("当前时间：" + DateTime.Now.ToString());
 
+            return Task.CompletedTask;
         }
     }
 }
