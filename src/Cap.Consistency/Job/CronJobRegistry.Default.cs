b5a0214ae5e38b223d79140fd0cd48db509d2780
yangxiaodong <yangxiaodong1214@126.com>
2017-06-21 16:06:53 +0800

    add DefaultCronJobRegistry about Cap Job.

---
+++ src/Cap.Consistency/Job/CronJobRegistry.Default.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Cap.Consistency.Infrastructure;
+using Microsoft.Extensions.Options;
+
+namespace Cap.Consistency.Job
+{
+    public class DefaultCronJobRegistry : CronJobRegistry
+    {
+        private readonly ConsistencyOptions _options;
+
+        public DefaultCronJobRegistry(IOptions<ConsistencyOptions> options) {
+            _options = options.Value;
+
+            RegisterJob<CapJob>(nameof(DefaultCronJobRegistry), _options.CronExp, RetryBehavior.DefaultRetry);
+        }
+    }
+}
