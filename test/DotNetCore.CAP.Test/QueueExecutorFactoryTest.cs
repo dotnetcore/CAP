af4b8e1a1004f865cec3c46f5265a816269e4c5a
yangxiaodong <yangxiaodong1214@126.com>
2017-07-18 14:26:19 +0800

    add unit test.

---
+++ test/DotNetCore.CAP.Test/QueueExecutorFactoryTest.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace DotNetCore.CAP.Test
+{
+    public class QueueExecutorFactoryTest
+    {
+        private IServiceProvider _provider;
+
+        public QueueExecutorFactoryTest()
+        {
+            var services = new ServiceCollection();
+            services.AddLogging();
+            services.AddOptions();
+            services.AddCap(x => { });
+            _provider = services.BuildServiceProvider();
+        }
+
+        [Fact]
+        public void CanCreateInstance()
+        {
+            var queueExecutorFactory = _provider.GetService<IQueueExecutorFactory>();
+            Assert.NotNull(queueExecutorFactory);
+
+            var publishExecutor = queueExecutorFactory.GetInstance(Models.MessageType.Publish);
+            Assert.Null(publishExecutor);
+
+            var disPatchExector = queueExecutorFactory.GetInstance(Models.MessageType.Subscribe);
+            Assert.NotNull(disPatchExector);
+        }
+
+        [Fact]
+        public void CanGetSubscribeExector()
+        {
+            var queueExecutorFactory = _provider.GetService<IQueueExecutorFactory>();
+            Assert.NotNull(queueExecutorFactory);
+
+            var publishExecutor = queueExecutorFactory.GetInstance(Models.MessageType.Publish);
+            Assert.Equal(null, publishExecutor);
+        }
+
+
+
+    }
+}
