123a3bbe3c65c6a7d4e53ea1ad0117ba97d7bb78
Savorboard <yangxiaodong1214@126.com>
2017-08-15 22:21:46 +0800

    add exception class.

---
+++ src/DotNetCore.CAP/Internal/MethodBindException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCore.CAP.Internal
+{
+    [Serializable]
+    public class MethodBindException : Exception
+    {
+        public MethodBindException() { }
+        public MethodBindException(string message) : base(message) { }
+        public MethodBindException(string message, Exception inner) : base(message, inner) { }     
+    }
+}
