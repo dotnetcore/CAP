499bc1020d19c3223865a4b01f91fb53e1636c4a
yangxiaodong <yangxiaodong1214@126.com>
2017-09-20 18:33:31 +0800

    add kafka sqlserver sample.

---
+++ samples/Sample.Kafka.SqlServer/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace Sample.Kafka.SqlServer
+{
+    public class Program
+    {
+
+        //var config = new ConfigurationBuilder()
+        //    .AddCommandLine(args)
+        //    .AddEnvironmentVariables("ASPNETCORE_")
+        //    .Build();
+
+        //var host = new WebHostBuilder()
+        //    .UseConfiguration(config)
+        //    .UseKestrel()
+        //    .UseContentRoot(Directory.GetCurrentDirectory())
+        //    .UseIISIntegration()
+        //    .UseStartup<Startup>()
+        //    .Build();
+
+        //host.Run();
+        public static void Main(string[] args)
+        { 
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .Build();
+
+    }
+}
---
+++ samples/Sample.Kafka.SqlServer/Startup.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Sample.Kafka.SqlServer
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<AppDbContext>();
+
+            services.AddCap(x =>
+            {
+                x.UseEntityFramework<AppDbContext>();
+                x.UseKafka("192.168.2.227:9091,192.168.2.227:9092,192.168.2.222:9092");
+                x.UseDashboard();
+                x.UseDiscovery(d =>
+                {
+                    d.DiscoveryServerHostName = "localhost";
+                    d.DiscoveryServerProt = 8500;
+                    d.CurrentNodeHostName = "localhost";
+                    d.CurrentNodePort = 5820;
+                    d.NodeName = "CAP 2号节点";
+                });
+            });
+
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+            loggerFactory.AddDebug();
+
+            app.UseMvc();
+
+            app.UseCap();
+
+            app.UseCapDashboard();
+        }
+    }
+}
