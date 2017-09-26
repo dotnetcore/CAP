7766d408c42169437453a1814538f5287786312d
Savorboard <yangxiaodong1214@126.com>
2017-09-26 23:17:31 +0800

    refactor and remove files.

--- src/DotNetCore.CAP/Dashboard/GatewayProxy/IRequestScopedDataRepository.cs
+++
@@ -1,9 +0,0 @@
-namespace DotNetCore.CAP.Dashboard.GatewayProxy
-{
-    public interface IRequestScopedDataRepository
-    {
-        void Add<T>(string key, T value);
-
-        T Get<T>(string key);
-    }
-}
