50bf7ddeb058fadbe660faf0aa2200e316182a35
yangxiaodong <yangxiaodong1214@126.com>
2017-09-27 11:56:34 +0800

    renamed message dto.

---
+++ src/DotNetCore.CAP/Models/CapMessageDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DotNetCore.CAP.Models
+{
+    public class CapMessageDto
+    {
+        public virtual string Id { get; set; }
+
+        public virtual DateTime Timestamp { get; set; }
+
+        public virtual object Content { get; set; }
+
+        public virtual string CallbackName { get; set; }
+
+        public CapMessageDto()
+        {
+            Id = ObjectId.GenerateNewStringId();
+            Timestamp = DateTime.Now;
+        }
+
+        public CapMessageDto(object content) : this()
+        {
+            Content = content;
+        }
+    }
+}
