03e012a8360c84a71708436e502018b18808fb01
Savorboard <yangxiaodong1214@126.com>
2017-11-02 20:30:46 +0800

    add summary comment.

--- src/DotNetCore.CAP/IConsumerClient.cs
+++ src/DotNetCore.CAP/IConsumerClient.cs
@@ -10,12 +10,25 @@ namespace DotNetCore.CAP
     /// </summary>
     public interface IConsumerClient : IDisposable
     {
+        /// <summary>
+        /// Subscribe to a set of topics to the message queue
+        /// </summary>
+        /// <param name="topics"></param>
         void Subscribe(IEnumerable<string> topics);
 
+        /// <summary>
+        /// Start listening
+        /// </summary>
         void Listening(TimeSpan timeout, CancellationToken cancellationToken);
 
+        /// <summary>
+        /// Manual submit message offset when the message consumption is complete
+        /// </summary>
         void Commit();
 
+        /// <summary>
+        /// Reject message and resumption
+        /// </summary>
         void Reject();
 
         event EventHandler<MessageContext> OnMessageReceived;
