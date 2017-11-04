c889ad8fc5b22beb473ea47bec312efead7b82d6
Savorboard <yangxiaodong1214@126.com>
2017-11-04 20:30:52 +0800

    add summary comment.

--- src/DotNetCore.CAP/IPublishExecutor.cs
+++ src/DotNetCore.CAP/IPublishExecutor.cs
@@ -2,8 +2,17 @@
 
 namespace DotNetCore.CAP
 {
+    /// <summary>
+    /// publish message excutor. The excutor sends the message to the message queue
+    /// </summary>
     public interface IPublishExecutor
     {
+        /// <summary>
+        /// publish message to message queue.
+        /// </summary>
+        /// <param name="keyName">The message topic name.</param>
+        /// <param name="content">The message content.</param>
+        /// <returns></returns>
         Task<OperateResult> PublishAsync(string keyName, string content);
     }
 }
