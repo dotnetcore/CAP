52dd95ffbb872df704a4f30084795018147342af
Savorboard <yangxiaodong1214@126.com>
2017-11-05 20:30:59 +0800

    add summary comment.

--- src/DotNetCore.CAP/MessageContext.cs
+++ src/DotNetCore.CAP/MessageContext.cs
@@ -1,11 +1,23 @@
 ﻿namespace DotNetCore.CAP
 {
+    /// <summary>
+    /// Message context
+    /// </summary>
     public class MessageContext
     {
+        /// <summary>
+        /// Gets or sets the message group.
+        /// </summary>
         public string Group { get; set; }
 
+        /// <summary>
+        /// Message name.
+        /// </summary>
         public string Name { get; set; }
 
+        /// <summary>
+        /// Message content
+        /// </summary>
         public string Content { get; set; }
 
         public override string ToString()
