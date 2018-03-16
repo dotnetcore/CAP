e2db01c30877b8ebbf09fdcd941cc939ecd48caf
Savorboard <yangxiaodong1214@126.com>
2018-03-16 18:17:04 +0800

    add exception class to process the publish send and subscriber exectution exception

---
+++ src/DotNetCore.CAP/Internal/PublisherSentFailedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCore.CAP.Internal
+{
+    public class PublisherSentFailedException : Exception
+    {
+        public PublisherSentFailedException(string message, Exception ex) : base(message, ex)
+        {
+
+        }
+    }
+}
---
+++ src/DotNetCore.CAP/Internal/SubscriberExecutionFailedException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DotNetCore.CAP.Internal
+{
+    internal class SubscriberExecutionFailedException : Exception
+    {
+        public SubscriberExecutionFailedException(string message, Exception ex) : base(message, ex)
+        {
+
+        }
+    }
+}
