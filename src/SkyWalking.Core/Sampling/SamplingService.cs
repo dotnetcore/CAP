b6ef70c85cf6ca0ef2e5a1b6b22b5a4080680c82
Liuhaoyang <liuhaoyang1221@hotmail.com>
2018-04-04 22:00:34 +0800

    Add SamplingService

---
+++ src/SkyWalking.Core/Sampling/SamplingService.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+namespace SkyWalking.Sampling
+{
+    public class SamplingService : ISampler
+    {
+        public Task Executing()
+        {
+            return Task.CompletedTask;
+        }
+
+        public Task Executed()
+        {
+            return Task.CompletedTask;
+        }
+
+        public bool TrySampling()
+        {
+            return true;
+        }
+
+        public void ForceSampled()
+        {
+        }
+    }
+}
