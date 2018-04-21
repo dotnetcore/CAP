30081daa20c3957639f5199972cb8d492236f6ea
Liuhaoyang <liuhaoyang1221@hotmail.com>
2018-04-21 23:24:08 +0800

    Fix license.Licensed all files under OpenSkywalking.

--- src/SkyWalking.Core/Context/IgnoredTracerContext.cs
+++ src/SkyWalking.Core/Context/IgnoredTracerContext.cs
@@ -1,5 +1,5 @@
 ﻿/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
+ * Licensed to the OpenSkywalking under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
  * The ASF licenses this file to You under the Apache License, Version 2.0
@@ -15,7 +15,6 @@
  * limitations under the License.
  *
  */
-
 
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
