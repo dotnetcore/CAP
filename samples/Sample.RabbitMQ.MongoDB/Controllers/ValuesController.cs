270ab4307efd81d7dba6b640025e7328796a5116
keke <keke@cnblogs.com>
2018-07-19 11:36:30 +0800

    Add sample for MongoDB

---
+++ samples/Sample.RabbitMQ.MongoDB/Controllers/ValuesController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetCore.CAP;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Sample.RabbitMQ.MongoDB.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IMongoClient _client;
+        private readonly ICapPublisher _capPublisher;
+
+        public ValuesController(IMongoClient client, ICapPublisher capPublisher)
+        {
+            _client = client;
+            _capPublisher = capPublisher;
+        }
+
+        [Route("~/publish")]
+        public IActionResult PublishWithSession()
+        {
+            using (var session = _client.StartSession())
+            {
+                session.StartTransaction();
+                var collection = _client.GetDatabase("TEST").GetCollection<BsonDocument>("test");
+                collection.InsertOne(new BsonDocument { { "hello", "world" } });
+
+                _capPublisher.Publish("sample.rabbitmq.mongodb", DateTime.Now, session);
+
+                session.CommitTransaction();
+            }
+            return Ok();
+        }
+    }
+}
---
+++ samples/Sample.RabbitMQ.MongoDB/Startup.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Sample.RabbitMQ.MongoDB
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IMongoClient>(new MongoClient(Configuration.GetConnectionString("MongoDB")));
+            services.AddCap(x =>
+            {
+                x.UseMongoDB();
+                x.UseRabbitMQ("localhost");
+            });
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.UseMvc();
+
+            app.UseCap();
+        }
+    }
+}
