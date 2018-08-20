2ec580648ac62a3e890ba36ad5c92e879d93d027
Savorboard <yangxiaodong1214@126.com>
2018-08-20 18:54:40 +0800

    Add RelationDbTransaction and extensions

---
+++ src/DotNetCore.CAP/Internal/RelationDbTransaction.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) .NET Core Community. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Data;
+
+namespace DotNetCore.CAP.Internal
+{
+    internal class RelationDbTransaction : IDbTransaction
+    {
+        private readonly ICapTransaction _capTransaction;
+
+        public RelationDbTransaction(ICapTransaction capTransaction)
+        {
+            _capTransaction = capTransaction;
+            var dbTransaction = (IDbTransaction) capTransaction.DbTransaction;
+            Connection = dbTransaction.Connection;
+            IsolationLevel = dbTransaction.IsolationLevel;
+        }
+
+        public void Dispose()
+        {
+            _capTransaction.Dispose();
+        }
+
+        public void Commit()
+        {
+            _capTransaction.Commit();
+        }
+
+        public void Rollback()
+        {
+            _capTransaction.Rollback();
+        }
+
+        public IDbConnection Connection { get; }
+        public IsolationLevel IsolationLevel { get; }
+    }
+}
---
+++ src/DotNetCore.CAP/TransactionExtensions.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) .NET Core Community. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Data;
+using DotNetCore.CAP.Internal;
+
+namespace DotNetCore.CAP
+{
+    public static class TransactionExtensions
+    {
+        public static ICapTransaction Begin(this ICapTransaction transaction,
+            IDbTransaction dbTransaction, bool autoCommit = false)
+        {
+
+            transaction.DbTransaction = dbTransaction;
+            transaction.AutoCommit = autoCommit;
+
+            return transaction;
+        }
+
+        public static IDbTransaction JoinToTransaction(this IDbTransaction dbTransaction,
+            ICapPublisher publisher, bool autoCommit = false)
+        {
+            dbTransaction = new RelationDbTransaction(publisher.Transaction.Begin(dbTransaction, autoCommit));
+            return dbTransaction;
+        }
+
+        public static IDbTransaction BeginAndJoinToTransaction(this IDbConnection dbConnection,
+            ICapPublisher publisher, bool autoCommit = false)
+        {
+            if (dbConnection.State == ConnectionState.Closed)
+            {
+                dbConnection.Open();
+            }
+
+            var dbTransaction = dbConnection.BeginTransaction();
+            return dbTransaction.JoinToTransaction(publisher, autoCommit);
+        }
+    }
+}
