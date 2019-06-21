11f6f5d2e8b89109c773689c23dc88dceb8619f3
Null <hetaoos@gmail.com>
2019-06-21 09:17:58 +0800

    DotNetCore.CAP.MongoDB: added index. (#353)

--- src/DotNetCore.CAP.MongoDB/IStorage.MongoDB.cs
+++ src/DotNetCore.CAP.MongoDB/IStorage.MongoDB.cs
@@ -59,7 +59,47 @@ namespace DotNetCore.CAP.MongoDB
                     cancellationToken: cancellationToken);
             }
 
+            var receivedMessageIndexNames = new string[] {
+                nameof(ReceivedMessage.Name), nameof(ReceivedMessage.Added), nameof(ReceivedMessage.ExpiresAt),
+                nameof(ReceivedMessage.StatusName), nameof(ReceivedMessage.Retries), nameof(ReceivedMessage.Version) };
+
+            var publishedMessageIndexNames = new string[] {
+                nameof(PublishedMessage.Name), nameof(PublishedMessage.Added), nameof(PublishedMessage.ExpiresAt),
+                nameof(PublishedMessage.StatusName), nameof(PublishedMessage.Retries), nameof(PublishedMessage.Version) };
+
+            await Task.WhenAll(
+                TryCreateIndexesAsync<ReceivedMessage>(_options.ReceivedCollection, receivedMessageIndexNames),
+                TryCreateIndexesAsync<PublishedMessage>(_options.PublishedCollection, publishedMessageIndexNames)
+                );
+
             _logger.LogDebug("Ensuring all create database tables script are applied.");
+
+            async Task TryCreateIndexesAsync<T>(string collectionName, string[] indexNames)
+            {
+                var col = database.GetCollection<T>(collectionName);
+                using (var cursor = await col.Indexes.ListAsync(cancellationToken))
+                {
+                    var existingIndexes = await cursor.ToListAsync(cancellationToken);
+                    var existingIndexNames = existingIndexes.Select(o => o["name"].AsString).ToArray();
+                    indexNames = indexNames.Except(existingIndexNames).ToArray();
+                }
+
+                if (indexNames.Any() == false)
+                    return;
+
+                var indexes = indexNames.Select(index_name =>
+                {
+                    var indexOptions = new CreateIndexOptions
+                    {
+                        Name = index_name,
+                        Background = true,
+                    };
+                    var indexBuilder = Builders<T>.IndexKeys;
+                    return new CreateIndexModel<T>(indexBuilder.Descending(index_name), indexOptions);
+                }).ToArray();
+
+                await col.Indexes.CreateManyAsync(indexes, cancellationToken);
+            }
         }
     }
 }
