d1c26b75bd644cee55029c0e9225b7fc029d4203
Savorboard <yangxiaodong1214@126.com>
2019-07-26 09:47:51 +0800

    Fixed asynchronous exception catching bug of sending.

--- src/DotNetCore.CAP/Processor/IDispatcher.Default.cs
+++ src/DotNetCore.CAP/Processor/IDispatcher.Default.cs
@@ -59,17 +59,17 @@ namespace DotNetCore.CAP.Processor
                 {
                     if (_publishedMessageQueue.TryTake(out var message, 3000, _cts.Token))
                     {
-                        try
+                        Task.Run(async () =>
                         {
-                            Task.Run(async () =>
+                            try
                             {
                                 await _sender.SendAsync(message);
-                            });
-                        }
-                        catch (Exception ex)
-                        {
-                            _logger.LogError(ex, $"An exception occurred when sending a message to the MQ. Topic:{message.Name}, Id:{message.Id}");
-                        }
+                            }
+                            catch (Exception ex)
+                            {
+                                _logger.LogError(ex, $"An exception occurred when sending a message to the MQ. Topic:{message.Name}, Id:{message.Id}");
+                            }
+                        });
                     }
                 }
             }
