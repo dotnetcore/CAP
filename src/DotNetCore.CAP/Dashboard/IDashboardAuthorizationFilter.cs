3c6b704bcb83d5ef31dbc26eae4a318d0fc3bcbc
Zhenyu,Liu <liuzhenyulive@live.com>
2019-08-28 23:12:30 +0800

    change DashboardMiddleware to async (#390)

--- src/DotNetCore.CAP/Dashboard/IDashboardAuthorizationFilter.cs
+++ src/DotNetCore.CAP/Dashboard/IDashboardAuthorizationFilter.cs
@@ -1,10 +1,12 @@
 ﻿// Copyright (c) .NET Core Community. All rights reserved.
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
+using System.Threading.Tasks;
+
 namespace DotNetCore.CAP.Dashboard
 {
     public interface IDashboardAuthorizationFilter
     {
-        bool Authorize(DashboardContext context);
+        Task<bool> AuthorizeAsync(DashboardContext context);
     }
 }
