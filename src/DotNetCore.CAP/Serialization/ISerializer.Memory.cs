3845698f2cbc4829b9a9bb84f087db25051e42d4
Savorboard <yangxiaodong1214@126.com>
2019-09-26 09:48:36 +0800

    Add new memory serializer

---
+++ src/DotNetCore.CAP/Serialization/ISerializer.Memory.cs
@@ -0,0 +1,32 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using DotNetCore.CAP.Messages;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace DotNetCore.CAP.Serialization
+{
+    public class MemorySerializer : ISerializer
+    {
+        public Task<TransportMessage> SerializeAsync(Message message)
+        {
+            var bf = new BinaryFormatter();
+            using (var ms = new MemoryStream())
+            {
+                bf.Serialize(ms, message.Value);
+                return Task.FromResult(new TransportMessage(message.Headers, ms.ToArray()));
+            }
+        }
+
+        public async Task<Message> DeserializeAsync(TransportMessage transportMessage)
+        {
+            using (var memStream = new MemoryStream())
+            {
+                var binForm = new BinaryFormatter();
+                await memStream.WriteAsync(transportMessage.Body, 0, transportMessage.Body.Length);
+                memStream.Seek(0, SeekOrigin.Begin);
+                var obj = binForm.Deserialize(memStream);
+                return new Message(transportMessage.Headers, obj);
+            }
+        }
+    }
+}
