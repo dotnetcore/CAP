59f90b07bb2e47edf3c1fd8068bf0ae610185178
Savorboard <yangxiaodong1214@126.com>
2019-11-05 18:22:17 +0800

    Add Nodediscovry

---
+++ src/DotNetCore.CAP.Dashboard/NodeDiscovery/CAP.DiscoveryOptions.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) .NET Core Community. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+namespace DotNetCore.CAP.Dashboard.NodeDiscovery
+{
+    public class DiscoveryOptions
+    {
+        public const string DefaultDiscoveryServerHost = "localhost";
+        public const int DefaultDiscoveryServerPort = 8500;
+
+        public const string DefaultCurrentNodeHostName = "localhost";
+        public const int DefaultCurrentNodePort = 5000;
+
+        public const string DefaultMatchPath = "/cap";
+
+        public DiscoveryOptions()
+        {
+            DiscoveryServerHostName = DefaultDiscoveryServerHost;
+            DiscoveryServerPort = DefaultDiscoveryServerPort;
+
+            CurrentNodeHostName = DefaultCurrentNodeHostName;
+            CurrentNodePort = DefaultCurrentNodePort;
+
+            MatchPath = DefaultMatchPath;
+        }
+
+        public string DiscoveryServerHostName { get; set; }
+        public int DiscoveryServerPort { get; set; }
+
+        public string CurrentNodeHostName { get; set; }
+        public int CurrentNodePort { get; set; }
+
+        public string NodeId { get; set; }
+        public string NodeName { get; set; }
+
+        public string MatchPath { get; set; }
+    }
+}
