cc7eb1062d2200999964f49d153c708fbfc3fc6d
Marko Zorec <mzorec@gmail.com>
2019-11-06 03:14:23 +0100

    Adds Flubu build script

---
+++ build/BuildScript.Version.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml;
+using FlubuCore.Context;
+using FlubuCore.Scripting.Attributes;
+
+namespace BuildScript
+{
+    [Include("./build/BuildVersion.cs")]
+    [Reference("System.Xml.XmlDocument, System.Xml, Version=4.0.0.0, Culture=neutral, publicKeyToken=b77a5c561934e089")]
+    public partial class BuildScript
+    {
+        public BuildVersion FetchBuildVersion(ITaskContext context)
+        {
+            var content = System.IO.File.ReadAllText("./build/version.props");
+
+            XmlDocument doc = new XmlDocument();
+            doc.LoadXml(content);
+
+            var versionMajor = doc.DocumentElement.SelectSingleNode("/Project/PropertyGroup/VersionMajor").InnerText;
+            var versionMinor = doc.DocumentElement.SelectSingleNode("/Project/PropertyGroup/VersionMinor").InnerText;
+            var versionPatch = doc.DocumentElement.SelectSingleNode("/Project/PropertyGroup/VersionPatch").InnerText;
+            var versionQuality = doc.DocumentElement.SelectSingleNode("/Project/PropertyGroup/VersionQuality").InnerText;
+            versionQuality = string.IsNullOrWhiteSpace(versionQuality) ? null : versionQuality;
+
+            var suffix = versionQuality;
+
+            bool isCi = false;
+            bool isTagged = false;
+            if (!context.BuildSystems().IsLocalBuild)
+            {
+                isCi = true;
+                //// todo use flubu build system when available.
+                var appveyortag = context.GetEnvironmentVariable("APPVEYOR_REPO_TAG");
+                bool isTagAppveyor = !string.IsNullOrEmpty(appveyortag) && appveyortag.Equals("true", StringComparison.OrdinalIgnoreCase);
+             
+                if ((context.BuildSystems().RunningOn == BuildSystemType.AppVeyor && isTagAppveyor) ||
+                    (context.BuildSystems().RunningOn == BuildSystemType.TravisCI && string.IsNullOrWhiteSpace(context.BuildSystems().Travis().TagName)))
+                {
+                    isTagged = true;
+                }
+            }
+
+
+            if (!isTagged)
+            {
+                suffix += (isCi ? "preview-" : "dv-") + CreateStamp();
+            }
+            suffix = string.IsNullOrWhiteSpace(suffix) ? null : suffix;
+
+            var version = new BuildVersion(int.Parse(versionMajor), int.Parse(versionMinor), int.Parse(versionPatch), versionQuality);
+            version.Suffix = suffix;
+
+            return version;
+        }
+    }
+}
---
+++ build/BuildScript.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FlubuCore.Context;
+using FlubuCore.Scripting;
+using FlubuCore.Scripting.Attributes;
+using GlobExpressions;
+
+namespace BuildScript
+{
+    [Include("./build/BuildVersion.cs")]
+    public partial class BuildScript : DefaultBuildScript
+    {
+        private const string ArtifactsDir = "./artifacts";
+
+        [FromArg("c|configuration")]
+        public string Configuration { get; set; }
+
+        protected BuildVersion BuildVersion { get; set; }
+
+        protected List<string> ProjectFiles { get; set; }
+
+        protected List<string> TestProjectFiles { get; set; }
+
+        protected override void ConfigureBuildProperties(IBuildPropertiesContext context)
+        {
+            context.Properties.Set(BuildProps.ProductId, "CAP");
+            context.Properties.Set(BuildProps.SolutionFileName, "CAP.sln");
+            context.Properties.Set(BuildProps.BuildConfiguration, string.IsNullOrEmpty(Configuration) ? "Release" : Configuration);
+            //// todo remove casting when new version of flubu is available
+            BuildVersion = FetchBuildVersion(context as ITaskContext);
+            Console.WriteLine(BuildVersion.Version());
+            TestProjectFiles =  Glob.Files("./test", "*/*.csproj", GlobOptions.MatchFullPath).Select(x => $"./test/{x}").ToList();
+            ProjectFiles = Glob.Files("./src", "*/*.csproj").Select(x => $"./src/{x}").ToList();
+        }
+
+        protected override void ConfigureTargets(ITaskContext context)
+        {
+            var clean = context.CreateTarget("Clean")
+                .AddCoreTask(x => x.Clean()
+                    .AddDirectoryToClean(ArtifactsDir, true));
+
+            var restore = context.CreateTarget("Restore")
+                .DependsOn(clean)
+                .AddCoreTask(x => x.Restore());
+
+            var build = context.CreateTarget("Build")
+                .DependsOn(restore)
+                .AddCoreTask(x => x.Build()
+                    .InformationalVersion(BuildVersion.VersionWithSuffix()));
+
+            var tests = context.CreateTarget("Tests")
+                .ForEach(TestProjectFiles,
+                    (projectFile, target) =>
+                    {
+                        target.AddCoreTask(x => x.Test()
+                            .Project(projectFile)
+                            .NoBuild());
+                    });
+
+          var pack =  context.CreateTarget("Pack")
+                .ForEach(ProjectFiles, (projectFile, target) =>
+                {
+                    target.AddCoreTask(x => x.Pack()
+                        .NoBuild()
+                        .Project(projectFile)
+                        .IncludeSymbols()
+                        .VersionSufix(BuildVersion.Suffix)
+                        .OutputDirectory(ArtifactsDir));
+                });
+
+          context.CreateTarget("Default")
+              .SetAsDefault()
+              .DependsOn(clean, restore, build, tests, pack);
+        }
+    }
+}
