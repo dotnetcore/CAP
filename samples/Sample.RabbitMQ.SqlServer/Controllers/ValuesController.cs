aedf82cbde3043cef9858b5a65c78f8e7b3f7fe7
Savorboard <yangxiaodong1214@126.com>
2019-12-05 14:00:44 +0800

    Add sqlsever samples

---
+++ samples/Sample.RabbitMQ.SqlServer/Controllers/ValuesController.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Data;
+using System.Threading.Tasks;
+using Dapper;
+using DotNetCore.CAP;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace Sample.RabbitMQ.SqlServer.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private readonly ICapPublisher _capBus;
+
+        public ValuesController(ICapPublisher capPublisher)
+        {
+            _capBus = capPublisher;
+        }
+
+        [Route("~/without/transaction")]
+        public async Task<IActionResult> WithoutTransaction()
+        {
+            await _capBus.PublishAsync("sample.rabbitmq.mysql", new Person()
+            {
+                Id = 123,
+                Name = "Bar"
+            });
+
+            return Ok();
+        }
+
+        [Route("~/adonet/transaction")]
+        public IActionResult AdonetWithTransaction()
+        {
+            using (var connection = new SqlConnection(AppDbContext.ConnectionString))
+            {
+                using (var transaction = connection.BeginTransaction(_capBus, true))
+                {
+                    //your business code
+                    connection.Execute("insert into test(name) values('test')", transaction: transaction);
+
+                    _capBus.Publish("sample.rabbitmq.mysql", DateTime.Now);
+                }
+            }
+
+            return Ok();
+        }
+
+        [Route("~/ef/transaction")]
+        public IActionResult EntityFrameworkWithTransaction([FromServices]AppDbContext dbContext)
+        {
+            using (dbContext.Database.BeginTransaction(_capBus, autoCommit: true))
+            {
+                dbContext.Persons.Add(new Person() { Name = "ef.transaction" });
+
+                _capBus.Publish("sample.rabbitmq.mysql", DateTime.Now);
+            }
+            return Ok();
+        }
+
+        [NonAction]
+        [CapSubscribe("sample.rabbitmq.mysql")]
+        public void Subscriber(DateTime p)
+        {
+            Console.WriteLine($@"{DateTime.Now} Subscriber invoked, Info: {p}");
+        }
+
+        [NonAction]
+        [CapSubscribe("sample.rabbitmq.mysql", Group = "group.test2")]
+        public void Subscriber2(DateTime p, [FromCap]CapHeader header)
+        {
+            Console.WriteLine($@"{DateTime.Now} Subscriber invoked, Info: {p}");
+        }
+    }
+}
