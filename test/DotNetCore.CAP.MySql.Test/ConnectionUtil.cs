1c43afb00eedf9996e129eca407721848d14968a
Savorboard <yangxiaodong1214@126.com>
2019-12-26 20:47:11 +0800

    Improve build script

--- test/DotNetCore.CAP.MySql.Test/ConnectionUtil.cs
+++ test/DotNetCore.CAP.MySql.Test/ConnectionUtil.cs
@@ -5,35 +5,29 @@ namespace DotNetCore.CAP.MySql.Test
 {
     public static class ConnectionUtil
     {
-        private const string DatabaseVariable = "Cap_MySql_DatabaseName";
-        private const string ConnectionStringTemplateVariable = "Cap_MySql_ConnectionStringTemplate";
+        private const string ConnectionStringTemplateVariable = "Cap_MySql_ConnectionString";
 
         private const string MasterDatabaseName = "information_schema";
-        private const string DefaultDatabaseName = @"DotNetCore.CAP.MySql.Test";
+        private const string DefaultDatabaseName = "cap_test";
 
-        private const string DefaultConnectionStringTemplate =
-            @"Server=localhost;Database={0};Uid=root;Pwd=123123;Allow User Variables=True;SslMode=none;";
+        private const string DefaultConnectionString =
+            @"Server=localhost;Database=cap_test;Uid=root;Pwd=123123;Allow User Variables=True;SslMode=none;";
 
         public static string GetDatabaseName()
         {
-            return Environment.GetEnvironmentVariable(DatabaseVariable) ?? DefaultDatabaseName;
+            return DefaultDatabaseName;
         }
 
         public static string GetMasterConnectionString()
         {
-            return string.Format(GetConnectionStringTemplate(), MasterDatabaseName);
+            return GetConnectionString().Replace(DefaultDatabaseName, MasterDatabaseName);
         }
 
         public static string GetConnectionString()
         {
-            return string.Format(GetConnectionStringTemplate(), GetDatabaseName());
-        }
-
-        private static string GetConnectionStringTemplate()
-        {
             return
                 Environment.GetEnvironmentVariable(ConnectionStringTemplateVariable) ??
-                DefaultConnectionStringTemplate;
+                DefaultConnectionString;
         }
 
         public static MySqlConnection CreateConnection(string connectionString = null)
