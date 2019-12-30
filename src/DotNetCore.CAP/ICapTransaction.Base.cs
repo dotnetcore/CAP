df3d28c92ae73df65e7afad99fdccd37bc513348
Savorboard <yangxiaodong1214@126.com>
2019-12-30 21:07:50 +0800

    Add file copyright

--- src/DotNetCore.CAP/ICapTransaction.Base.cs
+++ src/DotNetCore.CAP/ICapTransaction.Base.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Concurrent;
+﻿// Copyright (c) .NET Core Community. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for license information.
+
+using System.Collections.Concurrent;
 using System.Threading;
 using System.Threading.Tasks;
 using DotNetCore.CAP.Persistence;
