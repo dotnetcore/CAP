7928b360e92d0546ca989f509165ad8abdea9fa3
Savorboard <yangxiaodong1214@126.com>
2020-06-29 18:10:45 +0800

    Add AWS SQS Support. #597

---
+++ src/DotNetCore.CAP.AmazonSQS/TopicNormalizer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace DotNetCore.CAP.AmazonSQS
+{
+    public static class TopicNormalizer
+    {
+        public static string NormalizeForAws(this string origin)
+        {
+            if (origin.Length > 256)
+            {
+                throw new ArgumentOutOfRangeException(nameof(origin) + " character string length must between 1~256!");
+            }
+            return origin.Replace(".", "-").Replace(":", "_");
+        }
+
+        public static string DeNormalizeForAws(this string origin)
+        {
+            return origin.Replace("-", ".").Replace("_", ":");
+        }
+    }
+}
