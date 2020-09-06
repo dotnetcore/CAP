b563fd4ff89ffb3483fb116dbe935b55019e34dc
MysticBoy <mysticboy@live.com>
2020-09-06 23:19:24 +0800

    Modify namespaces and other variable names.

--- samples/Sample.ConsoleApp/Program.cs
+++ samples/Sample.ConsoleApp/Program.cs
@@ -21,7 +21,7 @@ namespace Sample.ConsoleApp
                     cfg.SubPort = 5556;
                     cfg.PubPort = 5557;
 
-                    cfg.Pattern = DotNetCore.CAP.ZeroMQ.NetMQPattern.PubSub;
+                    cfg.Pattern = DotNetCore.CAP.NetMQPattern.PubSub;
                 });
                 //x.UseRabbitMQ(cfg =>
                 //{
