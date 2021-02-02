c1c47330927bfba813c50eff8a9c6a05e22bd1ad
Savorboard <yangxiaodong1214@126.com>
2021-02-02 09:41:43 +0800

    Add options to create lazy queue for RabbitMQ. #772

--- src/DotNetCore.CAP.RabbitMQ/CAP.RabbiMQOptions.cs
+++ src/DotNetCore.CAP.RabbitMQ/CAP.RabbiMQOptions.cs
@@ -69,13 +69,27 @@ namespace DotNetCore.CAP
         public int Port { get; set; } = -1;
 
         /// <summary>
-        /// Gets or sets queue message automatic deletion time (in milliseconds). Default 864000000 ms (10 days).
+        /// Optional queue arguments, also known as "x-arguments" because of their field name in the AMQP 0-9-1 protocol,
+        /// is a map (dictionary) of arbitrary key/value pairs that can be provided by clients when a queue is declared.
         /// </summary>
-        public int QueueMessageExpires { get; set; } = 864000000;
+        public QueueArgumentsOptions QueueArguments { get; set; } = new QueueArgumentsOptions();
 
         /// <summary>
         /// RabbitMQ native connection factory options
         /// </summary>
         public Action<ConnectionFactory> ConnectionFactoryOptions { get; set; }
+
+        public class QueueArgumentsOptions
+        {
+            /// <summary>
+            /// Gets or sets queue mode by supplying the 'x-queue-mode' declaration argument with a string specifying the desired mode. Default 'default'
+            /// </summary>
+            public string QueueMode { get; set; } = "default";
+
+            /// <summary>
+            /// Gets or sets queue message automatic deletion time (in milliseconds) "x-message-ttl", Default 864000000 ms (10 days).
+            /// </summary>
+            public string MessageTTL { get; set; } = "864000000";
+        }
     }
 }
