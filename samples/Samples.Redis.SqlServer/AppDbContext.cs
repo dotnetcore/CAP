1522d1c2b1f76e2419d8a42ade80a1db28261138
mahmoud.samir <mahmoud.samir@desk.top>
2021-04-01 00:43:07 +0300

    add redis sample

---
+++ samples/Samples.Redis.SqlServer/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace Sample.Redis.SqlServer
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> optionsBuilder) : base(optionsBuilder)
+        {
+
+        }
+
+    }
+}
