64f6ee8c62a1b26084d0ace95d23d6a879ec9f16
luox78 <1023354041@qq.com>
2021-04-01 22:37:28 +0800

    Fix Dashboard metric not update #811

---
+++ src/DotNetCore.CAP.Dashboard/JsonSerializeOptions.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json;
+
+namespace DotNetCore.CAP.Dashboard
+{
+    public static class JsonSerializeOptions
+    {
+        public static JsonSerializerOptions Default = new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+    }
+}
