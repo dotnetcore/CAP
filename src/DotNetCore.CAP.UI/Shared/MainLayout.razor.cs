f6aa52fb14382c98b16c2247a6a0364fe13ce51f
Savorboard <yangxiaodong1214@126.com>
2021-05-07 00:08:52 +0800

    update mainlayout

--- src/DotNetCore.CAP.UI/Shared/MainLayout.razor.cs
+++ src/DotNetCore.CAP.UI/Shared/MainLayout.razor.cs
@@ -7,10 +7,6 @@ namespace DotNetCore.CAP.UI.Shared
 {
     public sealed partial class MainLayout
     {
-        private string Theme { get; set; } = "";
-
-        private bool IsFullSide { get; set; } = true;
-
         private List<MenuItem> Menus { get; set; }
 
         [Inject]
@@ -20,33 +16,33 @@ namespace DotNetCore.CAP.UI.Shared
         {
             base.OnInitialized();
 
-            Menus = GetIconSideMenuItems(Localizer);
+            Menus = GetMenus(Localizer);
         }
 
-        private static List<MenuItem> GetIconSideMenuItems(IStringLocalizer localizer)
+        private static List<MenuItem> GetMenus(IStringLocalizer localizer)
         {
-            var menus = new List<MenuItem>
-            {
-                new MenuItem() { Text = "返回组件库", Icon = "fa fa-fw fa-home", Url = "https://www.blazor.zone/components" },
-                new MenuItem() { Text = "Index", Icon = "fa fa-fw fa-fa", Url = "" },
-                new MenuItem() { Text = "Counter", Icon = "fa fa-fw fa-check-square-o", Url = "counter" },
-                new MenuItem() { Text = "FetchData", Icon = "fa fa-fw fa-database", Url = "fetchdata" },
-                new MenuItem() { Text = "Table", Icon = "fa fa-fw fa-table", Url = "table" }
-            };
-
             return new List<MenuItem>
             {
-                new(localizer["Published"].Value, icon: "fa fa-fa"),
+                new(localizer["Published"].Value, "/published/successed")
+                {
+                    IsActive = true,
+                    Items = new List<MenuItem>
+                    {
+                        new(localizer["Successed"].Value, "/published/successed"),
+                        new(localizer["Failed"].Value, "/published/failed")
+                    }
+                },
                 new(localizer["Received"].Value)
                 {
                     IsActive = true,
                     Items = new List<MenuItem>
                     {
-                        new(localizer["Successed"].Value),
-                        new(localizer["Failed"].Value)
+                        new(localizer["Successed"].Value, "/published/successed"),
+                        new(localizer["Failed"].Value, "/published/failed")
                     }
                 },
-                new(localizer["Menu3"].Value)
+                new(localizer["Subscribers"].Value, "/subscribers"),
+                new(localizer["Nodes"].Value, "/nodes")
             };
         }
     }
