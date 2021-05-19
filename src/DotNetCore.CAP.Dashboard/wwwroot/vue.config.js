3ad97eaf05283043805edd99bb29805b62cb4ed4
Savorboard <yangxiaodong1214@126.com>
2021-05-19 14:35:02 +0800

    update vue.config.js

--- src/DotNetCore.CAP.Dashboard/wwwroot/vue.config.js
+++ src/DotNetCore.CAP.Dashboard/wwwroot/vue.config.js
@@ -1,15 +1,6 @@
 module.exports = {
     publicPath: './',
     chainWebpack: config => {
-        // 移除 prefetch 插件
         config.plugins.delete('prefetch')
-
-        // 或者
-        //   // 修改它的选项：
-        //   config.plugin('prefetch').tap(options => {
-        //     options[0].fileBlacklist = options[0].fileBlacklist || []
-        //     options[0].fileBlacklist.push(/myasyncRoute(.)+?\.js$/)
-        //     return options
-        //   })
     }
 }
