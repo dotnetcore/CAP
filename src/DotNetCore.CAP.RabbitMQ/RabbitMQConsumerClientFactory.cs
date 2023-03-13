fef4311a955163a86a77cb6e09a86cbb4296ca94
revazashvili <levanrevazashvili@gmail.com>
2023-03-13 22:43:07 +0400

    remove unused directives

--- src/DotNetCore.CAP.RabbitMQ/RabbitMQConsumerClientFactory.cs
+++ src/DotNetCore.CAP.RabbitMQ/RabbitMQConsumerClientFactory.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT License. See License.txt in the project root for license information.
 
 using System;
-using System.Threading.Tasks;
-using DotNetCore.CAP.Messages;
 using DotNetCore.CAP.Transport;
 using Microsoft.Extensions.Options;
 
