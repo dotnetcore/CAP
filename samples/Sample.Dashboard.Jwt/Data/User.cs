0b3e372246f47ce69fe064c2d7cee8febcc181dd
Dimitry <dima.zhemkov@gmail.com>
2023-03-31 15:59:30 +0300

    Log in to dashboard with JWT authentication

---
+++ samples/Sample.Dashboard.Jwt/Data/User.cs
@@ -0,0 +1,8 @@
+﻿namespace Sample.Dashboard.Jwt.Data
+{
+    public class User
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
---
+++ samples/Sample.Dashboard.Jwt/wwwroot/js/site.js
@@ -0,0 +1,19 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function logIn() {
+  const userName = $('#userName').val();
+  const password = $('#password').val();
+  $.ajax({
+    method: "POST",
+    url: "security/createToken",
+    data: JSON.stringify({ userName, password }),
+    contentType: "application/json; charset=utf-8",
+    dataType: "json"
+  })
+    .done(token => {
+      window.open(`/cap/index.html?access_token=${token}`, '_blank')
+    });
+}
